e7f821ea27362888de979f7ef0211a65ed498b06
Analla Nayane <anallanayane09@gmail.com>
2018-11-19 13:57:00 -0300

    Organizando os Links

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -11,4 +11,29 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping("/login/login")
+	public String login() {
+		return "login/login";
+	}
+	
+	@RequestMapping("/user")
+	public String user() {
+		return "user";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil() {
+		return "perfil";
+	}
+	
+	@RequestMapping("/veiculos/meus_veiculos")
+	public String veiculos() {
+		return "veiculos/meus_veiculos";
+	}
+	
+	@RequestMapping("/servicos/meus_servicos")
+	public String servicos() {
+		return "servicos/meus_servicos";
+	}
+	
 }
